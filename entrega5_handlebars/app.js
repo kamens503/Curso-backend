dd8e1ed0e43be50de95c7db4433640116c9a8b84
Miguel Nunes <mens793@gmail.com>
2022-02-01 23:20:59 -0300

    Añadido las entregas 5

---
+++ entrega5_handlebars/app.js
@@ -0,0 +1,129 @@
+const express = require('express'),
+      { Router } = express,
+      api = Router(),
+      handlebars = require('express-handlebars')
+      Contenedor = require('./Contenedor.js'),
+      app = express(),
+      port = 8080 || process.env.port;
+
+const productos = new Contenedor('productos');
+
+
+app.engine(
+    "hbs",
+    handlebars({
+        extname: ".hbs",
+        defaultLayout: 'index.hbs',
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Bienvenidos la entrega 5: La entrega es en: <a href="http://localhost:${port}/api/productos">http://localhost:${port}/api/productos</a></h1>`)
+})
+
+api.get('/productos', (req, res) => {
+    console.log(productos.data);
+    const data = productos.data;
+    res.render('product_list',{data})
+
+});
+
+api.get('/subir_productos', (req, res) => {
+    data= '';
+    res.render('cargar_producto',{data})
+
+});
+
+api.get('/subir_productos/:status', (req, res) => {
+    console.log(req.params);
+    let data = {};
+    switch (req.params.status) {
+        case "no_title":
+            data = "Error: Falta título"
+            break;
+        case "no_price":
+            data = "Error: Falta precio"
+            break;
+        case "no_img":
+            data = "Error: Falta imagen"
+            break;
+        case "cargado":
+            data = "Producto cargado con exito"
+            break;
+        default:
+            break;
+    }
+    res.render('cargar_producto',{data})
+
+});
+api.get("/productos/:id", (req, res) => {
+
+    // console.log(req);
+    const key =  req.params.id;
+    console.log(productos.getById(key));
+    const data = {};
+    data.a = productos.getById(key);
+    res.render('product_list',{data})
+});
+
+
+api.post('/productos', (req, res) => {
+
+    console.log(req.body);
+    let product = {};
+    
+    product.title = req.body.title ? req.body.title 
+                                    : res.redirect("api/subir_productos/no_title");
+
+    product.price = req.body.price ? req.body.price 
+                                    : res.redirect("api/subir_productos/no_price");
+
+    product.thumbnail = req.body.thumbnail ? req.body.thumbnail 
+                                            : res.redirect("api/subir_productos/no_img");
+
+    productos.save(product);
+
+    console.log("Producto creado");
+    res.redirect("api/subir_productos/cargado");
+});
+
+
+api.put('/productos/:id', (req, res) => {
+    const id =req.params.id,
+          product = productos.getById(id);
+    
+    console.log(req.query);
+    
+    if(!product) res.status(404).send("No se consiguió el producto");
+
+    product.title = req.query.title ?  req.query.title : product.title;
+    product.price = req.query.price ?  req.query.price : product.price;
+    product.thumbnail = req.query.thumbnail ?  req.query.thumbnail : product.thumbnail;
+
+    productos.edit(id,product);
+    console.log("Producto actualizado");
+    res.send(product)
+});
+
+
+api.delete('/productos/:id', (req, res) => {
+    console.log(req.params);
+    const id =req.params.id,
+          product = productos.getById(id);
+
+    if(!product) res.status(404).send("No se consiguió el producto");
+
+    productos.deleteById(id);
+
+    res.status(200).send("Producto Borrado con éxito");
+});
+app.use(express.static(__dirname + "/public"));
+app.use("/api", api);
+app.listen(port, () => {
+    console.log(`Escuchando en http://localhost:${port}`)
+  }).on('error', e => {
+      console.log(e);
+  });
---
+++ entrega5_pug/app.js
@@ -0,0 +1,121 @@
+const express = require('express'),
+      { Router } = express,
+      api = Router(),
+      Contenedor = require('./Contenedor.js'),
+      app = express(),
+      port = 8080 || process.env.port;
+
+const productos = new Contenedor('productos');
+
+api.use(express.json());
+api.use(express.urlencoded({extended : true}));
+
+app.set('views','./views');
+app.set('view engine','pug');
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Bienvenidos la entrega 5: La entrega es en: <a href="http://localhost:${port}/api/productos">http://localhost:${port}/api/productos</a></h1>`)
+})
+
+api.get('/productos', (req, res) => {
+    console.log(productos.data);
+    const data = productos.data;
+    res.render('product_list.pug',{data})
+
+});
+
+api.get('/subir_productos', (req, res) => {
+    res.render('cargar_producto.pug')
+
+});
+
+api.get('/subir_productos/:status', (req, res) => {
+    console.log(req.params);
+    let data = {};
+    switch (req.params.status) {
+        case "no_title":
+            data = "Error: Falta título"
+            break;
+        case "no_price":
+            data = "Error: Falta precio"
+            break;
+        case "no_img":
+            data = "Error: Falta imagen"
+            break;
+        case "cargado":
+            data = "Producto cargado con exito"
+            break;
+        default:
+            break;
+    }
+    res.render('cargar_producto.pug',{data})
+
+});
+api.get("/productos/:id", (req, res) => {
+
+    // console.log(req);
+    const key =  req.params.id;
+    console.log(productos.getById(key));
+    const data = {};
+    data.a = productos.getById(key);
+    res.render('product_list.pug',{data})
+});
+
+
+api.post('/productos', (req, res) => {
+
+    console.log(req.body);
+    let product = {};
+    
+    product.title = req.body.title ? req.body.title 
+                                    : res.redirect("api/subir_productos/no_title");
+
+    product.price = req.body.price ? req.body.price 
+                                    : res.redirect("api/subir_productos/no_price");
+
+    product.thumbnail = req.body.thumbnail ? req.body.thumbnail 
+                                            : res.redirect("api/subir_productos/no_img");
+
+    productos.save(product);
+
+    console.log("Producto creado");
+    res.redirect("/subir_productos/cargado");
+});
+
+
+api.put('/productos/:id', (req, res) => {
+    const id =req.params.id,
+          product = productos.getById(id);
+    
+    console.log(req.query);
+    
+    if(!product) res.status(404).send("No se consiguió el producto");
+
+    product.title = req.query.title ?  req.query.title : product.title;
+    product.price = req.query.price ?  req.query.price : product.price;
+    product.thumbnail = req.query.thumbnail ?  req.query.thumbnail : product.thumbnail;
+
+    productos.edit(id,product);
+    console.log("Producto actualizado");
+    res.send(product)
+});
+
+
+api.delete('/productos/:id', (req, res) => {
+    console.log(req.params);
+    const id =req.params.id,
+          product = productos.getById(id);
+
+    if(!product) res.status(404).send("No se consiguió el producto");
+
+    productos.deleteById(id);
+
+    res.status(200).send("Producto Borrado con éxito");
+});
+app.use(express.static(__dirname + "/public"));
+app.use("/api", api);
+app.listen(port, () => {
+    console.log(`Escuchando en http://localhost:${port}`)
+  }).on('error', e => {
+      console.log(e);
+  });
